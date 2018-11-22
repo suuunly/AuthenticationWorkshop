40aece3fba92455562fb60fe8e33302719a01278
Jóhann Østerø <jost@ft.fo>
2018-11-22 13:58:34 +0000

    stuff

--- php/index.php
+++ php/index.php
@@ -1,13 +1,3 @@
-<?php
-       $dbhost = 'localhost:3306';
-       $dbuser = '';
-       $dbpass = '';
-       $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-       
-       if(! $conn ) {
-          die('Could not connect: ' . mysql_error());
-       }
-       
-       echo 'Connected successfully';
-       mysql_close($conn);
+<?php 
+echo hello
 ?>
